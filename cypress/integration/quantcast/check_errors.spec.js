80676fa1846a836e4920851b6a5ba46c2fc515b8
Robert Kuznetsov <robertkuznetsou@gmail.com>
2021-05-30 12:30:24 +0300

    Created test environment

---
+++ cypress/integration/quantcast/check_errors.spec.js
@@ -0,0 +1,6 @@
+
+context('Navigation to our website', () => {
+  it('Visit and check quantcast has error or not', () => {
+    cy.visit('https://www.nanoresort.com/stories/celebrities-lost-money-can-no-longer-afford-extravagant-lifestyles/');
+  })
+})
