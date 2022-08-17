6eb893fc35fa3bd49bd53c5e01a6e86de4ca9e19
Robert Kuznetsov <Robot72@users.noreply.github.com>
2022-08-17 15:16:29 +0300

    Changed URL address

--- cypress/integration/quantcast/check_errors.spec.js
+++ cypress/integration/quantcast/check_errors.spec.js
@@ -1,6 +1,6 @@
 
 context('Navigation to our website', () => {
   it('Visit and check quantcast has error or not', () => {
-    cy.visit('https://www.nanoresort.com/stories/celebrities-lost-money-can-no-longer-afford-extravagant-lifestyles/');
+    cy.visit('https://www.globetip.com/en/19592/?utm_source=taboola&utm_campaign=14984046&utm_content=3059625999&utm_medium=horizontimes-desktopt&utm_term=14984046_horizontimes-desktopt&utm_mb=xjm');
   })
 })
